01f7b668f490ae1226d111c32311ace583e7d1ac
ensar <ensarsagkal@gmail.com>
2023-12-06 10:22:14 +0300

    nextbutton js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import List from "./components/List"
+import { useState } from "react";
+import data from "./helper/data";
 
 function App() {
+  const[index,setIndex]= useState(5)
+  const nextFive=()=>{
+    if(index<data.length){
+    setIndex(index+5)}else{
+
+      setIndex(5)
+    }
+
+  }
+
   return (
-    <div>
-      <List/>
-    </div>
+    <main>
+      <section className="container">
+        <h2>Employee List</h2>
+      <List data={data} index={index}/>
+      <div className="buttons">
+        <button onClick={prevFive}>Previous</button>
+        <button onClick={nextFive}>Next</button>
+      </div>
+      </section>
+    </main>
   )
 }
 
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
-const List = () => {
+const List = ({data,index}) => {
   return (
-    <div>List</div>
+    <div>
+        {data.slice(index-5,index).map(({id,name,image,age,email})=>(
+            <div key={id} className='card'>
+                <img src={image} alt="" />
+              <div className='info'>
+              <h3>{name}</h3>
+                <h4>{email}</h4>
+                <h4>{age}</h4>
+              </div>
+
+            </div>
+        ))}
+    </div>
   )
 }
 
